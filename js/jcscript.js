12667a169edb5749a61b62d5af60faeb1d6ee3dd
llavillaccama <llavillaccama@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-29 00:56:32 +0000

    Actualize js comment submit
    git-svn-id: https://plugins.svn.wordpress.org/jc-recaptcha/trunk@1077867 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jcscript.js
+++ js/jcscript.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($){
 	$("form.wpcf7-form .wpcf7-submit").parent().prepend($(".g-recaptcha.jc.wp7"));
 
-	
-	$("input.submit,input#wp-submit,.submit-wrap input.ninja-forms-field,.wpcf7-submit").click(function(event) {
+
+	$("input.submit,input#wp-submit,.submit-wrap input.ninja-forms-field,.wpcf7-submit,.comment-form #submit").click(function(event) {
 		if ($("#g-recaptcha-response").val()=="") {
 			$(".jc-required").remove();
 			$(".g-recaptcha.jc").prepend('<p class="jc-required">Required</p>');
@@ -12,7 +12,7 @@ jQuery(document).ready(function($){
 		}
 	});
 
-  $("input.submit,input#wp-submit,.submit-wrap input.ninja-forms-field,.wpcf7-submit").click(function (event) {
+  $("input.submit,input#wp-submit,.submit-wrap input.ninja-forms-field,.wpcf7-submit,.comment-form #submit").click(function (event) {
         var com = $("textarea#comment").val();
         if (com == "") {
             event.preventDefault();
