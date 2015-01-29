bbabbd02cac946ec3a6ed88541a13e3c61b6f399
llavillaccama <llavillaccama@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-29 00:47:57 +0000

    Validate commet form
    git-svn-id: https://plugins.svn.wordpress.org/jc-recaptcha/trunk@1077863 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jcscript.js
+++ js/jcscript.js
@@ -1,18 +1,25 @@
 jQuery(document).ready(function($){
+	$("form.wpcf7-form .wpcf7-submit").parent().prepend($(".g-recaptcha.jc.wp7"));
+
+	
 	$("input.submit,input#wp-submit,.submit-wrap input.ninja-forms-field,.wpcf7-submit").click(function(event) {
 		if ($("#g-recaptcha-response").val()=="") {
 			$(".jc-required").remove();
 			$(".g-recaptcha.jc").prepend('<p class="jc-required">Required</p>');
 			event.preventDefault();
 		}else{
-			window.setTimeout('location.reload()', 500);
-			/*
-			jQuery("form").on('submitResponse', function(e, response){
-				if(typeof grecaptcha!="undefined"){
-					grecaptcha.reset();
-				}
-			});*/
+		grecaptcha.reset();
 		}
 	});
-	$("form.wpcf7-form .wpcf7-submit").parent().prepend($(".g-recaptcha.jc.wp7"));
+
+  $("input.submit,input#wp-submit,.submit-wrap input.ninja-forms-field,.wpcf7-submit").click(function (event) {
+        var com = $("textarea#comment").val();
+        if (com == "") {
+            event.preventDefault();
+        }
+        ;
+    });
+
+
+	
 });
